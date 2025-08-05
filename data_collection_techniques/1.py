466ec2381c2d1b44475b785e15524e9a3c64656d
sumit vikram <vikramsumit@outlook.com>
2025-08-05 23:15:11 +0530

    Initial commit

--- data_collection_techniques/1.py
+++ data_collection_techniques/1.py
@@ -1,9 +1,9 @@
-from bs4 import BeautifulSoup
+# from bs4 import BeautifulSoup
 
-html = "<html><body><h1>Hello</h1></body></html>"
-soup = BeautifulSoup(html, "html.parser")
+# html = "<html><body><h1>Hello</h1></body></html>"
+# soup = BeautifulSoup(html, "html.parser")
 
-print(soup.h1.text)
+# print(soup.h1.text)
 
-import bs4
-print(bs4.__version__)
+# import bs4
+# print(bs4.__version__)
